4a7f913f0fe03729f419a5fa8690eecce721db41
leofantucci <leo.fantucci@gmail.com>
2025-09-20 14:59:13 -0300

    commit 0

---
+++ src/main/Layout.java
@@ -0,0 +1,47 @@
+package main;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.GridLayout;
+import java.util.Scanner;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class Layout extends JFrame {
+	public Layout(int opcao) {
+		super("Layouts");
+		
+		Container c = getContentPane();
+		switch(opcao) {
+		case 1:
+			c.setLayout(new BorderLayout(15,15)); //Adiciona 15px de padding (espaçamento)
+			c.add(BorderLayout.NORTH, new JButton("N"));
+			c.add(BorderLayout.SOUTH, new JButton("S"));
+			c.add(BorderLayout.CENTER, new JButton("C"));
+			c.add(BorderLayout.EAST, new JButton("E"));
+			c.add(BorderLayout.WEST, new JButton("W"));
+			break;
+		
+
+		}
+		
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(300, 300);
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		int op;
+		System.out.println("Escolha o layout para ser visualizado:");
+		System.out.println("Border (1)");
+		System.out.println("Grid (2)");
+		
+		op = entrada.nextInt();
+		new Layout(op);
+		
+		entrada.close();
+	}
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module AulaWB {
+	requires java.desktop;
+}
